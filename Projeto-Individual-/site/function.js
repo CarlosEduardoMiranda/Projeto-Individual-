37b1b51b5de099a89571ab9a96eb18de458619b7
Carlos Eduardo <carlos.barbosa@sptech.school>
2023-11-10 23:24:53 -0300

    Organizada as pastas e feito o carrossel do site

---
+++ Projeto-Individual-/site/function.js
@@ -0,0 +1,48 @@
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+    nextImage();
+}, 5000);
+
+
+
+function nextImage(){
+  count++
+  if(count>4){
+    count = 1
+  }
+
+document.getElementById("radio"+count).checked = true;
+}

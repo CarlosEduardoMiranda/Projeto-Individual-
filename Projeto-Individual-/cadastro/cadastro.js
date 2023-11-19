787f5b879c9da46161810cc134276104cd00fcf2
CarlosEduardoMiranda <carlos.barbosa@sptech.school>
2023-11-19 18:34:08 -0300

    Adicionado a documentação, a modelagem e a parte da estatística do site

--- Projeto-Individual-/cadastro/cadastro.js
+++ Projeto-Individual-/cadastro/cadastro.js
@@ -1,21 +1,21 @@
 
 
 function cadastrar(){
-var nomeVar = firstname.value;
-var sobrenomeVar = lastname.value;
-var emailVar = email.value;
-var celularVar = number.value;
-var senhaVar = password.value;
-var confirmacaoSenhaVar = confirmpassword.value;
+var firstname = input_firstname.value;
+var lastname = input_lastname.value;
+var email = input_email.value;
+var number = input_number.value;
+var password = input_password.value;
+var confirmpassword = input_confirmpassword.value;
 
 if (
-  nomeVar == "" ||
-  sobrenomeVar == "" ||
-  emailVar == "" ||
-  celularVar == "" ||
-  senhaVar == "" ||
-  confirmacaoSenhaVar == ""
-) {
+  firstname == "" ||
+  lastname == "" ||
+  email == "" ||
+  number == "" ||
+  password == "" ||
+  confirmpassword == ""
+) { 
   cardErro.style.display = "block";
   mensagem_erro.innerHTML =
     "(Mensagem de erro para todos os campos em branco)";

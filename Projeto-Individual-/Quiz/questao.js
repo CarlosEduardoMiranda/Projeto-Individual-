1cb9ab0424b9247848988596e776839a9a596433
CarlosEduardoMiranda <carlos.barbosa@sptech.school>
2023-11-22 09:54:16 -0300

    Quiz feito

---
+++ Projeto-Individual-/Quiz/questao.js
@@ -0,0 +1,68 @@
+//Vetor de objetos
+var quiz = [
+
+    {
+      q: "Qual clube venceu a primeira edição da Copa Libertadores em 1960?",
+      opcoes: ['Santos', 'River Plate', 'Indepiendente', 'Penãrol'],
+      resposta: 0
+    },
+  
+    {
+      q: "Em que ano foi realizada a primeira final única da Copa Libertadores?",
+      opcoes: ['2017', '2018', '2019', '2016'],
+      resposta: 1
+    },   
+  
+    {
+      q: "Qual país não sediou a final da Copa Libertadores?",
+      opcoes: ['Uruguai', 'Paraguai', 'Equador', 'Chile'],
+      resposta: 0
+    },
+  
+    {
+      q: 'Quantas equipes participaram da primeira edição da Copa Libertadores em 1960?',
+      opcoes: ['8 equipes', '10 equipes', '12 equipes', '14 equipes'],
+      resposta: 2
+    },
+  
+    {
+      q: 'Quem foi considerado o melhor jogador da Libertadores em 2011?',
+      opcoes: ['Ganso', 'Emerson Sheik', 'Danilo', 'Neymar Jr'],
+      resposta: 3
+    },
+
+    {
+        q: "Quem é jogador brasileiro com mais gols da Libertadores?",
+        opcoes: ['Fred', 'Romário', 'Gabigol', 'Pelé'],
+        resposta: 2
+      },
+
+      {
+        q: "Qual jogador é conhecido como 'El Pibe de Oro' e venceu a Libertadores com o Boca Juniors?",
+        opcoes: ['Diego Maradona', 'Lionel Messi', 'Juan Román Riquelme', 'Lucas Pratto'],
+        resposta: 2
+      },
+
+
+      {
+        q: "Em que ano a Copa Libertadores teve sua primeira final entre times do mesmo país?",
+        opcoes: ['1998', '2000', '1999', '2003'],
+        resposta: 1
+      },
+
+      {
+        q: " Qual estádio sediou a final da Copa Libertadores em 2021?",
+        opcoes: ['Estádio Monumental', 'Maracanã', ' La Bombonera', 'Estádio Diego Armando Maradona'],
+        resposta: 0
+      },
+
+      {
+        q: "Quem é o uníco jogador brasileiro a ser tri-campeão da libertadores?",
+        opcoes: ['Gabigol', 'Felipe Melo', 'Weverton', 'Fábio Santos'],
+        resposta: 1
+      },
+
+
+
+
+]
